5164f30cba4647a36a0a2f6ad492914b960f05d3
Xanthom <vikneshdeivakumar7@gmail.com>
2024-02-17 20:44:24 +0530

    feat:Server script for concatentating machine code and parent fields and fetching them in machine_name

--- al_sulati/al_sulati/doctype/machine/machine.py
+++ al_sulati/al_sulati/doctype/machine/machine.py
@@ -3,7 +3,8 @@
 
 # import frappe
 from frappe.model.document import Document
-
-
 class Machine(Document):
-	pass
+	def before_save(self):
+		self.machine_name = f"{self.parent1} {self.machine_code or ''}"
+
+

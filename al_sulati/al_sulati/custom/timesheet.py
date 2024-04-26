63cab48a097ec8c94194d2a466ab6d279ee2a00d
Xanthom <vikneshdeivakumar7@gmail.com>
2024-04-26 22:55:15 +0530

    feat:fetching rate to timesheet(clientscript), made customization in serial and batchbundle and timesheet

--- al_sulati/al_sulati/custom/timesheet.py
+++ al_sulati/al_sulati/custom/timesheet.py
@@ -2,20 +2,21 @@ from frappe.model.document import Document
 import frappe
 import json
 
-
-
 @frappe.whitelist()
 def fetch_serial_no(doc):
     doc_obj = json.loads(doc)
+
     serial_no_doc = frappe.get_doc("Serial and Batch Bundle", doc_obj['custom_serial_no'])
     serial_no_details = serial_no_doc.get("entries")
+    project=frappe.get_doc("Project", doc_obj["parent_project"])
+    custom_rate=project.custom_rate
     serial_no_data = []
     data={}
     for  item in  serial_no_details:
         data = {
             "serial_no": item.serial_no,   
+            "custom_rate":custom_rate
         }
-
         serial_no_data.append(data)
 
     return serial_no_data

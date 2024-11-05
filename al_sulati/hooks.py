0a32234216f1f6872f362b2cdd8b4417178a608d
OmkarP-10 <omkarpangerkar10@gmail.com>
2024-11-05 16:41:00 +0530

    PO Creation Changes

--- al_sulati/hooks.py
+++ al_sulati/hooks.py
@@ -52,7 +52,11 @@ fixtures = [
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-doctype_js = {"Purchase Receipt": "public/js/purchase_receipt.js"}
+doctype_js = {
+    "Purchase Receipt": "public/js/purchase_receipt.js",
+    "Purchase Order": "public/js/purchase_order.js"  # Add this line
+}
+
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
 # doctype_calendar_js = {"doctype" : "public/js/doctype_calendar.js"}
